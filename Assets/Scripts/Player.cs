ba4de67ce9959ff0eafd9e640d0aff275ea3c32c
huyle333 <huyle333@bu.edu>
2019-04-17 09:47:26 -0700

    Created a single responsibility layout for Player, PlayerCombat, and PlayerMovement where Player controls the PlayerCombat and PlayerMovement.

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Player acts as a proxy for the interfaces of PlayerCombat and PlayerMovement!
+/// </summary>
+public class Player : MonoBehaviour
+{
+    // Single responsibility principle.
+    private PlayerCombat m_Combat;
+    private PlayerMovement m_Movement;
+
+    public PlayerCombat Combat
+    {
+        get { return m_Combat; }
+    }
+
+    public PlayerMovement Movement
+    {
+        get { return m_Movement; }
+    }
+
+    private void Start()
+    {
+        m_Combat = this.gameObject.AddComponent<PlayerCombat>();
+        m_Movement = this.gameObject.AddComponent<PlayerMovement>();
+    }
+}
---
+++ Assets/Scripts/PlayerCombat.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Handle combat-related responsibilities.
+/// </summary>
+public class PlayerCombat : MonoBehaviour
+{
+    private int m_Health;
+    private int m_MaxHealth;
+
+    public PlayerCombat(int health)
+    {
+        m_Health = health;
+        m_MaxHealth = health;
+    }
+
+    public int Health
+    {
+        get { return this.m_Health; }
+        set { this.m_Health = value; }
+    }
+
+    public int MaxHealth
+    {
+        get { return this.m_MaxHealth; }
+        set { this.m_MaxHealth = value; }
+    }
+
+    /// <summary>
+    /// Takes an integer and subtracts the current health from.
+    /// </summary>
+    /// <param name="amount"></param>
+    public void TakeDamage(int amount)
+    {
+        m_Health = Mathf.Max(0, Mathf.Max(0, Mathf.Min(m_MaxHealth, m_Health - amount));
+    }
+}
